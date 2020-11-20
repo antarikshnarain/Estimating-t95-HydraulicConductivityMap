cfd52df0b3efbf6322dd90d225b82aaebda815ff
Antariksh <antariksh.cloud@gmail.com>
2020-11-19 18:52:39 -0800

    const header file added.

---
+++ probabilistic_estimation/Const.hpp
@@ -0,0 +1,16 @@
+/*---------------------------------
+ * Author: Antariksh Narain
+ * Description: Header file storing all the constants.
+ *---------------------------------*/
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <stdio.h>
+#include <iomanip>
+
+#define MAP_ROWS 100
+#define MAP_COLS 205
+#define N_RECORDS 2060
+
+using namespace std;
